31b80efef2e70527b813607797e4ba91fe8812bf
Craig Knoblauch <craigknoblauch@gmail.com>
2018-09-11 19:40:15 -0700

    initial commit to begin Travis CI

---
+++ src/build_me.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/**
+ * The main function of this program. Simply confirms that the file has built successfully
+ * @return Default return for main function
+ */
+int main() {
+    printf("Successfully built and run");
+    return 0;
+}
